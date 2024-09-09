a62081a4c7e0b4eb088844469ee1ce7c5bf5b9f0
GO00OSE <Nathan.townsend.nct@gmail.com>
2024-09-10 08:02:25 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
-#include <cstddef>
+#include <iostream>
 #include <stdio.h>
 #include <Windows.h>
+#include <windef.h>
 #include <winuser.h>
 
 int main (int argc, char *argv[]) {
   RegisterHotKey(NULL, 1, MOD_ALT | MOD_CONTROL, 0x42);
   RegisterHotKey(NULL, 2, MOD_ALT | MOD_CONTROL, 0x46);
   MSG msg = {0};
+
   while (1) {
     if(GetMessage(&msg, NULL, 0, 0) != 0){
       if(msg.message == WM_HOTKEY){
@@ -16,6 +18,8 @@ int main (int argc, char *argv[]) {
             break;
           case 2:
             printf("CTRL ALT F\n");
+            HWND win = GetFocus();
+            printf("%s\n", GetWindowTextW(win, title, 500)); 
             break;
         }
       }
